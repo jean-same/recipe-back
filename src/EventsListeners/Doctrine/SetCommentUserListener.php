72e2189060984d47b56742cb3251854cdd74a287
jean-same <jnmauricesam92@gmail.com>
2022-03-25 21:30:55 +0100

    Mise en place d'un doctrine listener pour automatiquement associer l'utilisateur connecté au commentaire ajouté ou modifié

---
+++ src/EventsListeners/Doctrine/SetCommentUserListener.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\EventsListeners\Doctrine;
+
+use App\Entity\Comment;
+use Symfony\Component\Security\Core\Security;
+
+class SetCommentUserListener
+{
+
+    private $security;
+
+    public function __construct(Security $security)
+    {
+        $this->security = $security;
+    }
+
+    public function prePersist( Comment $comment)
+    {
+        $user = $this->security->getUser();
+        $comment->setUser($user);
+    }
+
+    public function preUpdate( Comment $comment)
+    { 
+        $user = $this->security->getUser();
+        $comment->setUser($user);
+    }
+}
